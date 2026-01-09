25d673611db8ee43a583bcc527af45979d3098d0
Auriane Marcelino <auriane.marcelino@gmail.com>
2026-01-09 20:04:27 +0100

    start README

--- README.md
+++ README.md
@@ -10,3 +10,16 @@ Auriane MARCELINO, DIA 2
 --- 
 
 ## How does the project works ?
+Go in your project folder
+Right click > Git Bash Here
+````bash
+mvn clean install
+````
+````bash
+chmod +x *.sh
+````
+````bash
+./integration_test.sh
+````
+Several logs will appear, do not care about them, they just explain all the actions that are processed in background.
+Among those logs, tabs will apear, they show the results of several functions :
--- integration_test.sh
+++ integration_test.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
-# # 1. Clean previous runs (Optional, to start fresh)
-# rm -rf bal_latest
-# rm -rf bal.db
-# rm -rf bal_processing_buffer
-# rm -rf bal_recomputed
+# 1. Clean previous runs (Optional, to start fresh)
+rm -rf bal_latest
+rm -rf bal.db
+rm -rf bal_processing_buffer
+rm -rf bal_recomputed
 
-# # 2. Run the loop for 10 days
-# for n in $(seq 0 10); do 
-#     day=$(date -d "2025-01-01 +${n} day" +%Y-%m-%d)
-#     echo ""
-#     echo "------------------------------------------------"
-#     echo "Processing Day $((n+1)): ${day}"
+# 2. Run the loop for 10 days
+for n in $(seq 0 10); do 
+    day=$(date -d "2025-01-01 +${n} day" +%Y-%m-%d)
+    echo ""
+    echo "------------------------------------------------"
+    echo "Processing Day $((n+1)): ${day}"
     
-#     # Input file path (must match where generate_data.sh put them)
-#     INPUT_FILE="data_generated/dump-${day}.csv"
+    # Input file path (must match where generate_data.sh put them)
+    INPUT_FILE="data_generated/dump-${day}.csv"
     
-#     # Run Feature 1: Integration
-#     ./run_daily_file_integration.sh "${day}" "${INPUT_FILE}"
+    # Run Feature 1: Integration
+    ./run_daily_file_integration.sh "${day}" "${INPUT_FILE}"
     
-#     # # Run Feature 2: Report
-#     # ./run_report.sh
-# done
+    # # Run Feature 2: Report
+    # ./run_report.sh
+done
 
-# echo "------------------------------------------------"
-# echo "Daily Integration Loop Finished."
+echo "------------------------------------------------"
+echo "Daily Integration Loop Finished."
 
-# # 3. Test Feature 3: Time Travel (Recompute Dump)
-# echo ""
-# echo "Testing Time Travel..."
-# for n in $(seq 0 10); do
-#     date=$(date -d "2025-01-01 +${n} day" +%Y-%m-%d)
-#     echo "Recomputing and extracting dump at date: ${date}"
-#     ./recompute_and_extract_dump_at_date.sh ${date} $"bal_recomputed/day=${date}"
-# done
+# 3. Test Feature 3: Time Travel (Recompute Dump)
+echo ""
+echo "Testing Time Travel..."
+for n in $(seq 0 10); do
+    date=$(date -d "2025-01-01 +${n} day" +%Y-%m-%d)
+    echo "Recomputing and extracting dump at date: ${date}"
+    ./recompute_and_extract_dump_at_date.sh ${date} $"bal_recomputed/day=${date}"
+done
 
 # 4. Test Feature 4: Diff
 dateA="2025-01-10"
